5127a781d8e942cd6434d9b403b6150ad8885d3c
rioperezg <113980673+rioperezg@users.noreply.github.com>
2023-03-22 16:57:56 +0100

    ejercicio1 apilando en aguja 1

--- Ejercicio1/1.Ejercicio1.py
+++ Ejercicio1/1.Ejercicio1.py
@@ -5,4 +5,16 @@ estaba debajo. A los sacerdotes se les encomendó la tarea de pasarlos todos des
 solo puede moverse un disco a la vez, y ningún disco podrá ponerse en- cima de otro más pequeño. Se dijo a los sacerdotes que, cuando 
 hubieran terminado de mover los discos, llegaría el fin del mundo. Resolver este problema de la Torre de Hanói.
 """
+from pila import Pila
+# Creamos tres pilas que corresponden a las tres agujas
+aguja1 = Pila()
+aguja2 = Pila()
+aguja3 = Pila()
+# Hacemos un bucle para que la aguja 1 tenga 74 datos apilados
+n = 74
+for dato in range(n):
+    Pila.apilar(aguja1, dato)
 
+print(Pila.pila_vacia(aguja1))
+print(Pila.en_cima(aguja1))
+print(Pila.Tamaño(aguja1))
