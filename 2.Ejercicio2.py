cd987e7232fc71e5c4a4595a95cc7ce01dcea7fb
rioperezg <113980673+rioperezg@users.noreply.github.com>
2023-03-22 19:42:45 +0100

    rehaciendo iterativo

--- 2.Ejercicio2.py
+++ 2.Ejercicio2.py
@@ -11,61 +11,17 @@ def Iter_sarrus(matrix):
             suma1 = pos[1]*pos[5]*pos[9]
 # segunda multiplicacion
         elif i == 2:
-            pos[2]*pos[6]*pos[7]
+            suma2 = pos[2]*pos[6]*pos[7]
 # tercera multipicaicon
         elif i == 3:
-            pos1 = pos
-            for j, pos in matrix:
-                if j == 4:
-                    pos2 = pos
-                    for k, pos in matrix:
-                        if k == 8:
-                            pos3 = pos
-                            suma3 = pos1*pos2*pos3
-                        else:
-                            continue
-                else:
-                   continue
+            suma3 = pos[3]*pos[4]*pos[8]
 #   lo mismo pero al reves:primera multiplicacion
         elif i == 4:
-            pos1 = pos
-            for j, pos in matrix:
-                if j == 2:
-                    pos2 = pos
-                    for k, pos in matrix:
-                        if k == 9:
-                            pos3 = pos
-                            resta1 = pos1*pos2*pos3
-                        else:
-                            continue
-                else:
-                   continue     
+            resta1 = pos[4]*pos[2]*pos[9]
         elif i == 5:
-            pos1 = pos
-            for j, pos in matrix:
-                if j == 3:
-                    pos2 = pos
-                    for k, pos in matrix:
-                        if k == 7:
-                            pos3 = pos
-                            resta2 = pos1*pos2*pos3
-                        else:
-                            continue
-                else:
-                   continue
+            resta2 = pos[5]*pos[3]*pos[7]
         elif i == 6:    
-            pos1 = pos
-            for j, pos in matrix:
-                if j == 1:
-                    pos2 = pos
-                    for k, pos in matrix:
-                        if k == 8:
-                            pos3 = pos
-                            resta3 = pos1*pos2*pos3
-                        else:
-                            continue
-                else:
-                   continue
+            resta3 = pos[6]*pos[1]*pos[8]
     return (suma1 + suma2 + suma3) - (resta1 + resta2 + resta3)
 matriz = [1, 3, 4, -1, -5, 4, 9, 0, -2]
 print(Iter_sarrus(matriz))
