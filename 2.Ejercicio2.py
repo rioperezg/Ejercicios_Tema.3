bc5dbe986f2139ae394eee57576482e66fc1cc8a
rioperezg <113980673+rioperezg@users.noreply.github.com>
2023-03-23 16:24:57 +0100

    Arreglando forma iterativa

--- 2.Ejercicio2.py
+++ 2.Ejercicio2.py
@@ -6,22 +6,28 @@ iterativa
 # Primero de forma iterativa
 def Iter_sarrus(matrix):
 # primera multiplicaicon diagonal
-    for i, pos in enumerate(matrix):
+    for i in matrix:
         if i == 1:
-            suma1 = pos[1]*pos[5]*pos[9]
+            suma1 = matrix[1]*matrix[5]*matrix[9]
 # segunda multiplicacion
         elif i == 2:
-            suma2 = pos[2]*pos[6]*pos[7]
+            suma2 = matrix[2]*matrix[6]*matrix[7]
 # tercera multipicaicon
         elif i == 3:
-            suma3 = pos[3]*pos[4]*pos[8]
+            suma3 = matrix[3]*matrix[4]*matrix[8]
 #   lo mismo pero al reves:primera multiplicacion
         elif i == 4:
-            resta1 = pos[4]*pos[2]*pos[9]
+            resta1 = matrix[4]*matrix[2]*matrix[9]
         elif i == 5:
-            resta2 = pos[5]*pos[3]*pos[7]
+            resta2 = matrix[5]*matrix[3]*matrix[7]
         elif i == 6:    
-            resta3 = pos[6]*pos[1]*pos[8]
+            resta3 = matrix[6]*matrix[1]*matrix[8]
     return (suma1 + suma2 + suma3) - (resta1 + resta2 + resta3)
-matriz = [1, 3, 4, -1, -5, 4, 9, 0, -2]
+
+
+matriz = [
+           [1, 3, 4]
+          ,[-1, -5, 4]
+          ,[9, 0, -2]
+]
 print(Iter_sarrus(matriz))
